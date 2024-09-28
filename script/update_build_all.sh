1866930d4328c0d2e23e1d79b3d53b4753259dc0
shuweic <shuweic227@gmail.com>
2024-09-28 15:41:23 -0500

    add script to gitignore

--- script/update_build_all.sh
+++ script/update_build_all.sh
@@ -5,6 +5,6 @@ PASSWORD="UIUCcui202408"
 for val in 0{1..9} 10
 do
     echo VM$val
-    sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no shuweic3@fa24-cs425-87$val.cs.illinois.edu "cd cs425_mp2_go/; git pull; go build; exit"
+    sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no shuweic3@fa24-cs425-87$val.cs.illinois.edu "cd cs425_mp2_go/; git stash; git pull; git stash clear; go build; exit"
 done
 echo 'Git Update!'
